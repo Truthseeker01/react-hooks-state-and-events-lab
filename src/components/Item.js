9de71cabcb3fce771383b7d980d2454d33e8256a
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-04-05 12:52:57 -0400

    lab complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useTransition } from "react";
 
 function Item({ name, category }) {
+  const [ inCart, setInCart ] = useState(false);
+
+  function updateInCart(){
+    setInCart(inCart => !inCart);
+  }
+  const liClass = (inCart) ? "in-cart" : "";
+  const btnText = (inCart) ? "Remove from Cart" : "Add to Cart"
+  const color = (inCart) ? "rgb(210, 51, 210)" : "rgb(237, 237, 32)";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" style={{background: color}} onClick={updateInCart}>{btnText}</button>
     </li>
   );
 }
